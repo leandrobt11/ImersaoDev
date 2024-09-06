ce60006295a284e9e9e63c996a22a3d785bc18bd
leandrobt11 <leandrobertto1106@gmail.com>
2024-09-05 22:18:33 -0300

    Aula2 - Estilizando o projeto com CSS./ Aula3 - Criando a base de dados com JavaScript

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        nome: "Gossip Girl",
+        ano: "2008",
+        descricao: "Gossip Girl é uma série de televisão americana que nos leva para o mundo luxuoso e cheio de intrigas da alta sociedade de Manhattan. Centrada na vida de um grupo de adolescentes privilegiados, a série nos apresenta a um jogo de poder, moda e romance, onde segredos e fofocas são a moeda corrente.",
+        link: "https://pt.wikipedia.org/wiki/Gossip_Girl_(telessérie)",
+        assistir :  "https://www.netflix.com/br/title/70143811"
+    },
+    {
+        nome: "Stranger Things",
+        ano: "2016",
+        descricao: "Stranger Things é uma série de ficção científica ambientada nos anos 80, que acompanha um grupo de amigos em busca de um amigo desaparecido, enquanto enfrentam forças sobrenaturais de outro mundo.",
+        link: "https://pt.wikipedia.org/wiki/Stranger_Things",
+        assistir: "https://www.netflix.com/br/title/80057281"
+    },
+    {
+        nome: "The witcher",
+        ano: "2019",
+        descricao: "The Witcher é uma série de fantasia épica baseada nos romances de Andrzej Sapkowski, que acompanha Geralt de Rívia, um caçador de monstros solitário, em um mundo medieval cheio de magia e criaturas perigosas.",
+        link: "https://pt.wikipedia.org/wiki/The_Witcher_(série_de_televisão)",
+        assistir: "https://www.netflix.com/br/title/80189685"
+    },
+    {
+        nome: "Cyberpunk: Edgerunners",
+        ano: "2022",
+        descricao: "Cyberpunk: Edgerunners é uma série de anime de cyberpunk ambientada no universo de Cyberpunk 2077, que acompanha um jovem que se torna um mercenário para sobreviver nas ruas de Night City.",
+        link: "https://pt.wikipedia.org/wiki/Cyberpunk:_Edgerunners",
+        assistir: "https://www.netflix.com/br/title/81054853"
+    }
+
+];
+
